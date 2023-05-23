839bdc4fe9eda8df14de7ee9a2565a4b21d3b23e
fekher-turki <turki.fekher@gmail.com>
2023-05-23 13:17:22 +0100

    Adding image file upload

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -18,6 +18,7 @@ use Symfony\Component\Form\Extension\Core\Type\RangeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Security\Core\Security;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class RecipeType extends AbstractType
 {
@@ -135,6 +136,12 @@ class RecipeType extends AbstractType
                 ],
                 'constraints' => [
                     new Assert\NotNull()
+                ]
+            ])
+            ->add('imageFile', VichImageType::class, [
+                'label' => 'Image de la recette',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
                 ]
             ])
             ->add('ingredients', EntityType::class, [
